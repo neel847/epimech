e21ad5f95f69870b0923c2d6ccb889002e5c9b49
Purav2003 <shahpurav308@gmail.com>
2025-04-22 17:08:29 -0400

    refactor: remove unnecessary semicolons and clean up formatting in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import withPWA from 'next-pwa';
+import withPWA from 'next-pwa'
 
 /** @type {import('next').NextConfig} */
 const baseConfig = {
@@ -9,15 +9,20 @@ const baseConfig = {
         hostname: 'picsum.photos',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'epimech.s3.us-east-2.amazonaws.com',
+        pathname: '/**',
+      },
     ],
   },
-};
+}
 
 const pwaConfig = {
   dest: 'public',
   register: true,
   skipWaiting: true,
   // disable: process.env.NODE_ENV === 'development',
-};
+}
 
-export default withPWA(pwaConfig)(baseConfig);
+export default withPWA(pwaConfig)(baseConfig)
