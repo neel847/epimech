cba58d735d073f6f1d4a20ba9d97784980f17419
Purav2003 <shahpurav308@gmail.com>
2025-04-26 18:13:55 -0400

    Add sitemap and Workbox service worker files
    - Created a new sitemap file (sitemap-0.xml) with URLs for the website.
    - Added Workbox service worker file (workbox-4754cb34.js) for enhanced caching and offline capabilities.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://epimech.com', // Your main domain
-    generateRobotsTxt: true,        // (automatically create robots.txt)
-    sitemapSize: 5000,
-    changefreq: 'daily',
-    priority: 0.7,
+    siteUrl: 'https://epimech.com', // ✅ Your website domain
+    generateRobotsTxt: true,        // ✅ Auto create robots.txt
+    sitemapSize: 7000,              // ✅ Safe limit if you have many products
+    changefreq: 'daily',            // ✅ Default: update frequency
+    priority: 0.7,                  // ✅ Priority of all pages
+    generateIndexSitemap: true,     // ✅ Will create a sitemap index if you have multiple sitemaps
   };
   
